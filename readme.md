1cd63b4946d78422f6244247b99e3efba24dcedd
mput <putintsev@gmail.com>
2019-03-18 16:55:00 +0300

    First commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+## LikeSlack - simple chat application.
---
+++ server/bin/slack.js
@@ -0,0 +1,4 @@
+import getApp from '..';
+
+const port = process.env.PORT || 5656;
+getApp().listen(port, () => console.log(`port: ${port}`));
