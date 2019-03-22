21390e8adb77e6e6f32078a24f6da5c6a894192f
mput <putintsev@gmail.com>
2019-03-22 14:54:18 +0300

    Mooved gon to externals

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import gon from 'gon';
+
 import ChatApp from './ChatApp';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,5 +11,5 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const { channels } = window.gon;
+const { channels } = gon;
 ReactDOM.render(<ChatApp channels={channels} />, document.getElementById('chatApp'));
