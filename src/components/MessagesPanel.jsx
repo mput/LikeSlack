7c34455d5b1ce0787f8c777e415d05876fc064e0
mput <putintsev@gmail.com>
2019-03-24 22:57:51 +0300

    Messages are now scrolled to the last item each time new one came.

--- src/components/MessagesPanel.jsx
+++ src/components/MessagesPanel.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card } from 'react-bootstrap';
-import ScrollableFeed from 'react-scrollable-feed';
 import Message from './Message';
 import MessageForm from './MessageForm';
 import { activeChannelMessages } from '../selectors';
@@ -11,14 +10,28 @@ const mapStateToProps = state => ({
 });
 
 // eslint-disable-next-line
-const MessagesPanel = ({ messages }) =>{
-  return (
-    <Card className="flex-fill h-100 overflow-auto">
-      <Card.Header>
-        <strong>#general</strong>
-      </Card.Header>
-      <Card.Body className="d-flex flex-column px-4 py-0 overflow-auto">
-        <ScrollableFeed>
+class MessagesPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.scrolTo = React.createRef();
+  }
+
+  componentDidMount() {
+    this.scrolTo.current.scrollIntoView();
+  }
+
+  componentDidUpdate() {
+    this.scrolTo.current.scrollIntoView();
+  }
+
+  render() {
+    const { messages } = this.props;
+    return (
+      <Card className="flex-fill h-100 overflow-auto">
+        <Card.Header>
+          <strong>#general</strong>
+        </Card.Header>
+        <Card.Body className="d-flex flex-column px-4 py-0 overflow-auto">
           {messages.map(({ message, author, id }, index) => (
             <Message
               message={message}
@@ -27,11 +40,12 @@ const MessagesPanel = ({ messages }) =>{
               key={id}
             />
           ))}
-        </ScrollableFeed>
-      </Card.Body>
-      <MessageForm />
-    </Card>
-  );
-};
+          <div ref={this.scrolTo} />
+        </Card.Body>
+        <MessageForm />
+      </Card>
+    );
+  }
+}
 
 export default connect(mapStateToProps)(MessagesPanel);
